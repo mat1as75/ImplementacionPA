6c6d197fe73fe46bf803a5c62fe4fcd3dd60d0fc
mat1as75 <matialfaro757575@gmail.com>
2024-08-26 16:48:21 -0300

    Actualizacion de Archivos

---
+++ Espotify/src/espotify/presentacion/AltaPerfil.java
@@ -0,0 +1,270 @@
+package espotify.presentacion;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import javax.swing.ImageIcon;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class AltaPerfil extends javax.swing.JInternalFrame {
+    public AltaPerfil() {
+        initComponents();
+        jLabelBiografia.setVisible(false);
+        jTextAreaBiografia.setVisible(false);
+        jScrollPane2.setVisible(false);
+        jLabelwebpromocion.setVisible(false);
+        jTextFieldwebpromocion.setVisible(false);
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jComboBoxUsuario = new javax.swing.JComboBox<>();
+        jPanel1 = new javax.swing.JPanel();
+        jLabelnickname = new javax.swing.JLabel();
+        jLabelnombre = new javax.swing.JLabel();
+        jLabelapellido = new javax.swing.JLabel();
+        jLabelemail = new javax.swing.JLabel();
+        jLabelfechanacimiento = new javax.swing.JLabel();
+        jTextFieldnickname = new javax.swing.JTextField();
+        jTextFieldnombre = new javax.swing.JTextField();
+        jTextFieldapellido = new javax.swing.JTextField();
+        jTextFieldemail = new javax.swing.JTextField();
+        openButton = new javax.swing.JButton();
+        imageLabel = new javax.swing.JLabel();
+        jLabelBiografia = new javax.swing.JLabel();
+        jLabelwebpromocion = new javax.swing.JLabel();
+        jTextFieldwebpromocion = new javax.swing.JTextField();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextAreaBiografia = new javax.swing.JTextArea();
+        jComboBoxusuario = new javax.swing.JComboBox<>();
+        jDateChooserFechaNacimiento = new com.toedter.calendar.JDateChooser();
+
+        jComboBoxUsuario.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        setClosable(true);
+        setIconifiable(true);
+        setMaximizable(true);
+        setResizable(true);
+        setTitle("Alta de Perfil");
+
+        jLabelnickname.setText("nickname");
+
+        jLabelnombre.setText("nombre");
+
+        jLabelapellido.setText("apellido");
+
+        jLabelemail.setText("email");
+
+        jLabelfechanacimiento.setText("fecha de nacimiento");
+
+        jTextFieldapellido.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextFieldapellidoActionPerformed(evt);
+            }
+        });
+
+        openButton.setText("Abrir imagen");
+        openButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                openButtonActionPerformed(evt);
+            }
+        });
+
+        imageLabel.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+
+        jLabelBiografia.setText("Biografia");
+
+        jLabelwebpromocion.setText("web de promocion");
+
+        jTextAreaBiografia.setColumns(20);
+        jTextAreaBiografia.setRows(5);
+        jScrollPane2.setViewportView(jTextAreaBiografia);
+
+        jComboBoxusuario.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "cliente", "artista" }));
+        jComboBoxusuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBoxusuarioActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(26, 26, 26)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabelnickname)
+                    .addComponent(jLabelnombre)
+                    .addComponent(jLabelapellido)
+                    .addComponent(jLabelemail)
+                    .addComponent(jLabelfechanacimiento)
+                    .addComponent(openButton)
+                    .addComponent(jLabelBiografia)
+                    .addComponent(jLabelwebpromocion))
+                .addGap(34, 34, 34)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jTextFieldwebpromocion, javax.swing.GroupLayout.PREFERRED_SIZE, 274, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap(167, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(jDateChooserFechaNacimiento, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jTextFieldemail, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jTextFieldapellido, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 176, Short.MAX_VALUE)
+                            .addComponent(jTextFieldnombre, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jTextFieldnickname, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(imageLabel, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jComboBoxusuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(92, 92, 92))))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabelnickname)
+                    .addComponent(jTextFieldnickname, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jComboBoxusuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabelnombre)
+                    .addComponent(jTextFieldnombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabelapellido)
+                    .addComponent(jTextFieldapellido, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabelemail)
+                    .addComponent(jTextFieldemail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(14, 14, 14)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabelfechanacimiento)
+                    .addComponent(jDateChooserFechaNacimiento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(imageLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(openButton))
+                .addGap(28, 28, 28)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabelBiografia)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabelwebpromocion))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTextFieldwebpromocion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(21, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextFieldapellidoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextFieldapellidoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextFieldapellidoActionPerformed
+
+    private void openButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_openButtonActionPerformed
+        JFileChooser buscar = new JFileChooser();
+        FileNameExtensionFilter extension = new FileNameExtensionFilter("Seleccionar imagen", "jpg", "png");
+        buscar.setFileFilter(extension);
+
+        if (buscar.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = buscar.getSelectedFile();
+            String ruta = selectedFile.getAbsolutePath();
+            // Configura la carpeta de destino
+            File destinoCarpeta = new File("./src/imagenesPerfil");
+            if (!destinoCarpeta.exists()) {
+                destinoCarpeta.mkdirs(); // Crea la carpeta si no existe
+            }
+
+            // Define el nuevo archivo en la carpeta de destino
+            //esto es lo que se guarda en la base de datos
+            File destinoArchivo = new File(destinoCarpeta, selectedFile.getName());
+            
+            try {
+                // Copia el archivo a la carpeta de destino
+                Files.copy(selectedFile.toPath(), destinoArchivo.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                
+                // Cargar la imagen para mostrarla en el JLabel
+                Toolkit tool = Toolkit.getDefaultToolkit();
+                Image imagen = tool.createImage(ruta);
+                imageLabel.setIcon(new ImageIcon(imagen.getScaledInstance(imageLabel.getWidth(), imageLabel.getHeight(), Image.SCALE_AREA_AVERAGING)));
+
+            } catch (IOException e) {
+                e.printStackTrace(); // Manejo de excepciones en caso de error al copiar
+            };
+        };
+            }//GEN-LAST:event_openButtonActionPerformed
+
+    private void jComboBoxusuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBoxusuarioActionPerformed
+        
+        String opcion=(String)jComboBoxusuario.getSelectedItem();
+        if("cliente".equals(opcion)){
+            jLabelBiografia.setVisible(false);
+            jTextAreaBiografia.setVisible(false);
+            jScrollPane2.setVisible(false);
+            jLabelwebpromocion.setVisible(false);
+            jTextFieldwebpromocion.setVisible(false);
+        }else{
+            jLabelBiografia.setVisible(true);
+            jTextAreaBiografia.setVisible(true);
+            jScrollPane2.setVisible(true);
+            jLabelwebpromocion.setVisible(true);
+            jTextFieldwebpromocion.setVisible(true);
+        }    
+       
+    }//GEN-LAST:event_jComboBoxusuarioActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel imageLabel;
+    private javax.swing.JComboBox<String> jComboBoxUsuario;
+    private javax.swing.JComboBox<String> jComboBoxusuario;
+    private com.toedter.calendar.JDateChooser jDateChooserFechaNacimiento;
+    private javax.swing.JLabel jLabelBiografia;
+    private javax.swing.JLabel jLabelapellido;
+    private javax.swing.JLabel jLabelemail;
+    private javax.swing.JLabel jLabelfechanacimiento;
+    private javax.swing.JLabel jLabelnickname;
+    private javax.swing.JLabel jLabelnombre;
+    private javax.swing.JLabel jLabelwebpromocion;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextArea jTextAreaBiografia;
+    private javax.swing.JTextField jTextFieldapellido;
+    private javax.swing.JTextField jTextFieldemail;
+    private javax.swing.JTextField jTextFieldnickname;
+    private javax.swing.JTextField jTextFieldnombre;
+    private javax.swing.JTextField jTextFieldwebpromocion;
+    private javax.swing.JButton openButton;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ Espotify/src/espotify/presentacion/PublicarLista.java
@@ -0,0 +1,106 @@
+package espotify.presentacion;
+
+import javax.swing.JScrollPane;
+public class PublicarLista extends javax.swing.JInternalFrame {
+    public PublicarLista() {
+        initComponents();
+        
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanelPublicarLista = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jTextFieldnickUsuario = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArealistaPublica = new javax.swing.JTextArea();
+        jToggleButtonenviar = new javax.swing.JToggleButton();
+
+        setClosable(true);
+        setIconifiable(true);
+        setMaximizable(true);
+        setResizable(true);
+        setTitle("Publicar Lista");
+
+        jLabel1.setText("nickname de usuario");
+
+        jLabel2.setText("lista que se hará pública");
+
+        jTextArealistaPublica.setColumns(20);
+        jTextArealistaPublica.setRows(5);
+        jScrollPane1.setViewportView(jTextArealistaPublica);
+
+        jToggleButtonenviar.setText("enviar");
+        jToggleButtonenviar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButtonenviarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanelPublicarListaLayout = new javax.swing.GroupLayout(jPanelPublicarLista);
+        jPanelPublicarLista.setLayout(jPanelPublicarListaLayout);
+        jPanelPublicarListaLayout.setHorizontalGroup(
+            jPanelPublicarListaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelPublicarListaLayout.createSequentialGroup()
+                .addGap(17, 17, 17)
+                .addGroup(jPanelPublicarListaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel1))
+                .addGap(18, 18, 18)
+                .addGroup(jPanelPublicarListaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jTextFieldnickUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(143, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelPublicarListaLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jToggleButtonenviar)
+                .addGap(227, 227, 227))
+        );
+        jPanelPublicarListaLayout.setVerticalGroup(
+            jPanelPublicarListaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelPublicarListaLayout.createSequentialGroup()
+                .addGap(12, 12, 12)
+                .addGroup(jPanelPublicarListaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jTextFieldnickUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(31, 31, 31)
+                .addGroup(jPanelPublicarListaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(jToggleButtonenviar)
+                .addContainerGap(146, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanelPublicarLista, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanelPublicarLista, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jToggleButtonenviarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButtonenviarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jToggleButtonenviarActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanelPublicarLista;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArealistaPublica;
+    private javax.swing.JTextField jTextFieldnickUsuario;
+    private javax.swing.JToggleButton jToggleButtonenviar;
+    // End of variables declaration//GEN-END:variables
+}
