9bebf760d3b54d1219792e727d124f9c99b563f3
mat1as75 <matialfaro757575@gmail.com>
2024-08-29 15:25:13 -0300

    Dejo un unico Controlador en Capa Logica

---
+++ Espotify/src/espotify/logica/Controlador.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package espotify.logica;
+
+/**
+ *
+ * @author tecnologo
+ */
+public class Controlador extends IControlador{
+
+    // 1 de Singleton
+    private Controlador() {
+        
+    }
+    
+    // 2 de Singleton
+    private static Controlador miInstancia = null;
+    
+    // 3 de Singleton
+    public static Controlador getInstance() {
+        if (Controlador.miInstancia == null) {
+            Controlador.miInstancia = new Controlador();
+        }
+        return (Controlador.miInstancia);
+    }
+}
