d772ecea3ad652d8fbbf36dc25a24d698be6be9d
elhenryford <henry.ford@estudiantes.utec.edu.uy>
2024-09-13 19:36:44 -0300

    Publicar lista agregue combox

--- Espotify/src/espotify/presentacion/PublicarLista.java
+++ Espotify/src/espotify/presentacion/PublicarLista.java
@@ -8,13 +8,24 @@ import javax.swing.JScrollPane;
 public class PublicarLista extends javax.swing.JInternalFrame {
     public PublicarLista() {
         initComponents();
-        String cliente = jTextFieldnickUsuario.getText();
         Fabrica f = Fabrica.getInstance();
         IControlador i = f.getControlador();
+        List<String> clientes=i.getNicknamesClientes();
+        for (String c : clientes) {
+            this.jComboBoxNickName.addItem(c);
+        }
+        
+        
+        String cliente =(String)jComboBoxNickName.getSelectedItem();
         List<String> privadotrue = i.listasCreadasEstadoPrivadoTrue(cliente);
         for (String p : privadotrue) {
+JOptionPane.showMessageDialog(null, "entro al for privado.", "Error", JOptionPane.ERROR_MESSAGE);
             jComboBoxListaSeraPublica.addItem(p);
         }
+
+
+
+
     }
 
     @SuppressWarnings("unchecked")
@@ -23,10 +34,10 @@ public class PublicarLista extends javax.swing.JInternalFrame {
 
         jPanelPublicarLista = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jTextFieldnickUsuario = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         jButtonEnviar = new javax.swing.JButton();
         jComboBoxListaSeraPublica = new javax.swing.JComboBox<>();
+        jComboBoxNickName = new javax.swing.JComboBox<>();
 
         setClosable(true);
         setIconifiable(true);
@@ -35,12 +46,6 @@ public class PublicarLista extends javax.swing.JInternalFrame {
         setTitle("Publicar Lista");
 
         jLabel1.setText("nickname de usuario");
-
-        jTextFieldnickUsuario.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextFieldnickUsuarioActionPerformed(evt);
-            }
-        });
 
         jLabel2.setText("lista que se hará pública");
 
@@ -61,10 +66,10 @@ public class PublicarLista extends javax.swing.JInternalFrame {
                     .addComponent(jLabel2)
                     .addComponent(jLabel1))
                 .addGap(18, 18, 18)
-                .addGroup(jPanelPublicarListaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jTextFieldnickUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jComboBoxListaSeraPublica, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(247, Short.MAX_VALUE))
+                .addGroup(jPanelPublicarListaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jComboBoxListaSeraPublica, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jComboBoxNickName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(345, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelPublicarListaLayout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButtonEnviar)
@@ -76,7 +81,7 @@ public class PublicarLista extends javax.swing.JInternalFrame {
                 .addGap(12, 12, 12)
                 .addGroup(jPanelPublicarListaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(jTextFieldnickUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jComboBoxNickName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(31, 31, 31)
                 .addGroup(jPanelPublicarListaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel2)
@@ -101,24 +106,20 @@ public class PublicarLista extends javax.swing.JInternalFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButtonEnviarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonEnviarActionPerformed
-        String cliente=this.jTextFieldnickUsuario.getText();
+        String cliente =(String)jComboBoxNickName.getSelectedItem();
         String lista=(String)this.jComboBoxListaSeraPublica.getSelectedItem();
         Fabrica f = Fabrica.getInstance();
         IControlador i = f.getControlador();
         i.setPrivadafalse(cliente,lista);
     }//GEN-LAST:event_jButtonEnviarActionPerformed
 
-    private void jTextFieldnickUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextFieldnickUsuarioActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextFieldnickUsuarioActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButtonEnviar;
     private javax.swing.JComboBox<String> jComboBoxListaSeraPublica;
+    private javax.swing.JComboBox<String> jComboBoxNickName;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanelPublicarLista;
-    private javax.swing.JTextField jTextFieldnickUsuario;
     // End of variables declaration//GEN-END:variables
 }
