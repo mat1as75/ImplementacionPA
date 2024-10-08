067ba5990e74ef703507d1fb5815147f29fa2a71
mat1as75 <matialfaro757575@gmail.com>
2024-10-07 22:02:53 -0300

    .

--- ServidorCentral/build/generated-sources/ap-source-output/espotify/logica/Album_.java
+++ ServidorCentral/build/generated-sources/ap-source-output/espotify/logica/Album_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-10-07T18:33:59", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-10-07T22:01:44", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Album.class)
 public class Album_ { 
 
--- ServidorCentral/build/generated-sources/ap-source-output/espotify/logica/Usuario_.java
+++ ServidorCentral/build/generated-sources/ap-source-output/espotify/logica/Usuario_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-10-07T18:33:59", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-10-07T22:01:44", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Usuario.class)
 public abstract class Usuario_ { 
 
