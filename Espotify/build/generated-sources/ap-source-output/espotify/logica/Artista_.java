ec87357eac5fc478e48e725368fc7ca319fc4765
smate0 <m4tesilva@gmail.com>
2024-10-19 20:54:41 -0300

    .

---
+++ Espotify/build/generated-sources/ap-source-output/espotify/logica/Artista_.java
@@ -0,0 +1,17 @@
+package espotify.logica;
+
+import espotify.logica.Album;
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-10-06T22:30:53", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Artista.class)
+public class Artista_ extends Usuario_ {
+
+    public static volatile SingularAttribute<Artista, String> dirSitioWeb;
+    public static volatile ListAttribute<Artista, Album> misAlbumesPublicados;
+    public static volatile SingularAttribute<Artista, String> biografia;
+
+}
---
+++ Espotify/build/generated-sources/ap-source-output/espotify/logica/Genero_.java
@@ -0,0 +1,21 @@
+package espotify.logica;
+
+import espotify.logica.Album;
+import espotify.logica.Genero;
+import espotify.logica.ListaPorDefecto;
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-10-06T22:30:53", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Genero.class)
+public class Genero_ { 
+
+    public static volatile ListAttribute<Genero, ListaPorDefecto> misListasParticulares;
+    public static volatile SingularAttribute<Genero, Genero> miPadre;
+    public static volatile SingularAttribute<Genero, String> nombreGenero;
+    public static volatile ListAttribute<Genero, Album> misAlbumes;
+    public static volatile ListAttribute<Genero, Genero> misGenerosHijos;
+
+}
