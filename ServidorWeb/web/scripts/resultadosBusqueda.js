ef6bc4322396cdf3f82b945a37f54bb0d8d5d677
mat1as75 <matialfaro757575@gmail.com>
2024-10-30 19:15:31 -0300

    Actualizo mi rama

--- ServidorWeb/web/scripts/resultadosBusqueda.js
+++ ServidorWeb/web/scripts/resultadosBusqueda.js
@@ -52,16 +52,63 @@ function mostrarResultados(resultados) {
     }
 }
 
-const orderByAlphabetical = (array, getter, order = 'asc') => {
-    array.sort((a, b) => {
-        const first = getter(a);
-        const second = getter(b);
+//const orderByAlphabetical = (array, getter, order = 'asc') => {
+//    array.sort((a, b) => {
+//        const first = getter(a);
+//        const second = getter(b);
+//        
+//        const compare = first.localeCompare(second);
+//        return order === 'asc' ? compare : -compare;
+//    });
+//    return array;
+//};
+
+//document.getElementById('ordenar').addEventListener('change', function() {
+//    const selectedOption = this.value;
+//    let sortedResults;
+//    
+//    if (selectedOption === 'alfabetico') {
+//        sortedResults = resultados.sort((a, b) => a.localeCompare(b));
+//    } else {
+//        sortedResults = resultados.sort((a, b) => a.anio - b.anio);
+//    }
+//    
+//    mostrarResultados(sortedResults);
+//});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sortOption = document.getElementById('ordenar');
+    
+    console.log('Resultados iniciales: ', resultados);
+    
+    sortOption.addEventListener('change', function() {
+        const selectedOption = this.value;
         
-        const compare = first.localeCompare(second);
-        return order === 'asc' ? compare : -compare;
+        // Crear un nuevo objeto para guardar los resultados ordenados
+        const sortedResults = {};
+        
+        for (const value in resultados) {
+            if (resultados.hasOwnProperty(value)) {
+                // Convertir el string a un arreglo
+                let arreglo = resultados[value];
+                
+                if (selectedOption === 'alfabetico') {
+                    // Ordenar alfabeticamente
+                    arreglo.sort((a, b) => a.localeCompare(b));
+                } else if (selectedOption === 'anio') {
+                    // Asumiento que los strings pueden representarse como anios (ej. '2020', '2021')
+                    arreglo.sort((a, b) => parseInt(a) - parseInt(b));
+                }
+                
+                // Asignar el arreglo ordenado al nuevo objeto
+                sortedResults[value] = arreglo.join();
+            }
+        }
+        
+        // Asegurarse de que esta funcion maneja la estructura de sortedResults
+        mostrarResultados(sortedResults);
     });
-    return array;
-};
+});
 
 //function ordenarResultados(resultados) {
 //    const select = document.getElementById('opciones');
