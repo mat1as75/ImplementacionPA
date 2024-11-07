560fb9b3790cf0518ca946ab4d9d6e515ca4d3d4
Matías Píriz <93398088+MatP25@users.noreply.github.com>
2024-11-07 14:34:29 -0300

    probando web services

---
+++ ServidorWeb/src/java/webservices/WSPrueba.java
@@ -0,0 +1,20 @@
+package webservices;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+@WebService(serviceName = "WSPrueba")
+@SOAPBinding(style = Style.RPC)
+public class WSPrueba {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
