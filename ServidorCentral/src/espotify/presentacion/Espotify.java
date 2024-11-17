ea1aa90f7b0259887865da48ad633c2d49531a49
mat1as75 <matialfaro757575@gmail.com>
2024-11-17 12:46:37 -0300

    Revert 7baf0e4

--- ServidorCentral/src/espotify/presentacion/Espotify.java
+++ ServidorCentral/src/espotify/presentacion/Espotify.java
@@ -3,8 +3,12 @@ package espotify.presentacion;
 import espotify.logica.Fabrica;
 import espotify.logica.IControlador;
 import espotify.persistencia.ControladoraPersistencia;
+<<<<<<< HEAD
 import espotify.webservices.PreferenciasService;
 import espotify.webservices.SuscripcionesService;
+=======
+import webservices.NuevoWebService;
+>>>>>>> parent of 5226f67 (termine con)
 
 public class Espotify extends javax.swing.JFrame {
 
@@ -19,7 +23,10 @@ public class Espotify extends javax.swing.JFrame {
         SuscripcionesService SuscripcionesWS = new SuscripcionesService();
         SuscripcionesWS.publishEndpoint();
         
+<<<<<<< HEAD
         
+=======
+>>>>>>> parent of 5226f67 (termine con)
         Fabrica fabrica = Fabrica.getInstance();
         ICtrl = fabrica.getControlador();
     }
